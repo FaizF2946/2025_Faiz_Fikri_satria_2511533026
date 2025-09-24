aebb476a83c5110420e685cf42d2f10712a58b5c
Lenovo <Lenovo@DESKTOP-ROFA39M>
2025-09-24 15:59:10 +0700

    Pekan 3

---
+++ prakalpro_2025_D_2511533026/src/Pekan_tiga/OperatorAritmatika.java
@@ -0,0 +1,32 @@
+package Pekan_tiga;
+
+import java.util.Scanner;
+
+public class OperatorAritmatika {
+    public static void main (String[] args) {
+    	int A1;
+    	int A2;
+    	int hasil;
+    	Scanner keyboard = new Scanner (System.in) ;
+    	System.out.println("Input angka-1: ");
+    	A1 = keyboard.nextInt();
+    	System.out.print("Input angka-2: ");
+    	A2 = keyboard.nextInt ();
+    	keyboard.close ();
+    	System.out.println("operator Penjumlahan");
+    	hasil = A1 + A2; //penjumlahan
+    	System.out.println("Hasil = " + hasil);
+    	System.out.println("operator Pengurangan");
+    	hasil = A1 - A2;//pengurangan
+    	System.out.println("Hasil = " + hasil);
+    	System.out.println("p[erasi Perkalian");
+    	hasil = A1 * A2;//perkalian
+    	System.out.println("Hasil = " + hasil);
+    	System.out.println("operator hasil bagi");
+    	hasil = A1 / A2;//pembagian
+    	System.out.println("Hasil = " + hasil);
+    	System.out.println("operator sisa bagi");
+    	hasil = A1 % A2;//Sisa Bagi
+    	System.out.println("Hasil = " + hasil);    	
+    }
+}
